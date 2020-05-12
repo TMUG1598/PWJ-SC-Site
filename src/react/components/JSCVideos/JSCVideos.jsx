bc1a58852ae15c356352793d1044b0ee29b03379
Tatyana Gonzaga <tmugonzaga@gmail.com>
2020-05-13 01:35:01 +0800

    Revert "Added Video Gallery"
    This reverts commit 56c8a8d9bc17631d36ec315e8277f056e54e7df8.

--- src/react/components/JSCVideos/JSCVideos.jsx
+++ src/react/components/JSCVideos/JSCVideos.jsx
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import VideoCard from './VideoCard';
-import styled from 'styled-components';
 // GET https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLmcLAMi0ZBwJ5cs92ppNp_-UeDDSXQtSW&key=[YOUR_API_KEY] HTTP/1.1
 
-const JSCVideos = ({ playListId }) => {
+const JSCVideos = () => {
   const [videos, setVideos] = useState([]);
+
   const getPlaylistItems = () => {
     const apiKey = 'AIzaSyDUwgBPrArU-r2TMEH0DX5PYTiVHT3oExI';
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playListId}&key=${apiKey}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const playlistItems = data.items.map((item) => item.snippet);
-        setVideos(playlistItems);
-      });
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLmcLAMi0ZBwJ5cs92ppNp_-UeDDSXQtSW&key=${apiKey}`;
+    fetch(url).then((data) => console.log(data));
     console.log('getPlayListItems');
   };
 
@@ -23,34 +16,10 @@ const JSCVideos = ({ playListId }) => {
   }, []);
 
   return (
-    <div id="videos">
-      <Heading>Video Gallery</Heading>
-      <VideoContainer>
-        {videos.map((video) => {
-          return (
-            <VideoCard
-              key={video.resourceId.videoId}
-              id={video.resourceId.videoId}
-              description={video.description}
-              thumbnail={video.thumbnails.high.url}
-              title={video.title}
-              playListId={playListId}
-            />
-          );
-        })}
-      </VideoContainer>
+    <div>
+      <h1>JS SUCCESS COACH VIDEOS Test</h1>
     </div>
   );
 };
-
-const Heading = styled.h1`
-  text-align: center;
-`;
-
-export const VideoContainer = styled.div`
-  display: flex;
-  padding: 1rem;
-  flex-wrap: wrap;
-`;
 
 export default JSCVideos;
