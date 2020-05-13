c3440f089c6474992e76d113f1c0e1b6977c770c
Sen Gaurav <sennngau@ap.dtcs.local>
2020-05-13 18:38:58 +0530

    updated slack profile and fixed time display and coach status logic to consider real time handling

--- script.js
+++ script.js
@@ -2,153 +2,172 @@ var coaches = [
   {
     name: "Fizan",
     UTC: "-7",
-    diff: -7,
+    diff: { hours: -7, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
-    slack:
-      "https://app.slack.com/client/T011D0D7T54/D012JMC0RH9/thread/C011D0D7WDC-1588478636.190000",
+    slack: { userId: "U011EKCLJKE", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Kiska",
     UTC: "-6",
-    diff: -6,
+    diff: { hours: -6, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011ZGK9T70", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
-    'name': 'Jenn',
-    'UTC': '-5',
-    'diff': -5,
-    'available': [9, 17],
-    'profpic': 'img/Jenn.png',
-    'description': "I am a frontend developer with expertise in CSS styling, data visualizations (using d3.js and chart.js), and UI/UX design. I am a bit quirky with a touch of fanciful imagination and a lover of shiny tech gadgets. I belive sharing memes is a true love language. I aspire to make the world a better place one smile at a time. My pronouns are She/Her.",
+    name: 'Jenn',
+    UTC: '-5',
+    diff: { hours: -5, minutes: 0 },
+    available: [9, 17],
+    profpic: 'img/Jenn.png',
+    description: "I am a frontend developer with expertise in CSS styling, data visualizations (using d3.js and chart.js), and UI/UX design. I am a bit quirky with a touch of fanciful imagination and a lover of shiny tech gadgets. I belive sharing memes is a true love language. I aspire to make the world a better place one smile at a time. My pronouns are She/Her.",
+    slack: { userId: "U012QP89WTB", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Osman",
     UTC: "-5",
-    diff: -5,
+    diff: { hours: -5, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U0132DUPHCY", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Jessica",
     UTC: "-5",
-    diff: -5,
+    diff: { hours: -5, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011CNDCVSB", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Rishi",
     UTC: "-4",
-    diff: -4,
+    diff: { hours: -4, minutes: 0 },
     available: [9, 17],
     profpic: "img/Rishi.jpg",
     description:
       "I am a 21 year old developer who just graduated in Mobile Application Design and Development and I am based in Canada for now. I have expertise in JavaScript, React and I am very enthusiast about Augmented Reality as I am working on one project based on AR and I love how machine learning and deep learning is changing th world. Enrolling in PWJ is one of the best thing I did in my life and I swear I mean it. Becoming Success Coach is kinda first achievement in my field in my life and I am vert proud of myself for that. This is the best community and I look forward to teach and learn as much as I can.",
+    slack: { userId: "U0123Q83D6X", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Youssef",
     UTC: "+1",
-    diff: 1,
+    diff: { hours: 1, minutes: 0 },
     available: [9, 17],
     profpic: "img/Yousseff.JPG",
     description:
       "I am a JavaScript Developer and a Microsoft Student Partner. I am passionate about CODING and new technologies. AND OF COURSE I LOVE PIZZA",
-    slack: "https://app.slack.com/client/T011D0D7T54/D012QM7Q6MP",
+    slack: { userId: "U012C8ZKXNF", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Abdou",
     UTC: "+2",
-    diff: 2,
+    diff: { hours: 2, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011B8BFZBR", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "David",
     UTC: "+2",
-    diff: 2,
+    diff: { hours: 2, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011LV52PMG", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Muizz",
     UTC: "+2",
-    diff: 2,
+    diff: { hours: 2, minutes: 0 },
     available: [9, 17],
     profpic: "img/Muizz.jpg",
     description: "I'm originally from Nigeria but I've lived in  France and currently in Germany where I'm  working as a Product Owner in a sports company. I'm a stickler for web technologies and love solving problems with coding. Always interested in anything video games, web development, and travelling.",
+    slack: { userId: "U011ZTC6SEM", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Gaurav",
     UTC: "+5:30",
-    diff: 5,
+    diff: { hours: 5, minutes: 30 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011D000RNZ", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Van",
     UTC: "+7",
-    diff: 7,
+    diff: { hours: 7, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U012DA935HA", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Pisoth",
     UTC: "+7",
-    diff: 7,
+    diff: { hours: 7, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U012ACS7FUG", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Damir",
     UTC: "+8",
-    diff: 8,
+    diff: { hours: 8, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U011CNAAEP9", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Tatyana",
     UTC: "+8",
-    diff: 8,
+    diff: { hours: 8, minutes: 0 },
     available: [9, 17],
     profpic: "img/Taty.jpg",
     description: "Currently located in the Philippines, I graduated in Chemistry and Material Science Engineering. Coding has always been something I thought I could do but this is the first time I really feel like I belong in the digital world. I love algorithms, rubick's cubesss, coffee, and surfing! I've also been told I am a good teacher, no matter the subject! *wink*",
+    slack: { userId: "U012BAWNUG5", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
   {
     name: "Ushan",
     UTC: "+12",
-    diff: 12,
+    diff: { hours: 12, minutes: 0 },
     available: [9, 17],
     profpic: "img/js-sample-pic.png",
     description: "I am me.",
+    slack: { userId: "U0119RT1A5P", profileSearchUrl: "https://app.slack.com/client/T011D0D7T54/browse-people/user_profile/" }
   },
 ];
-
+var intervalId;
 function displayLists(btn) {
   btn.classList.toggle("active");
 
-  let coachlist = listCoaches();
-  let timelist = listTimeZones();
-  let statuslist = listStatus();
-  let contactlist = listContact();
+
 
   if (btn.classList.contains("active") == true) {
-    // This to change the text in the button when clicked
+    const coachlist = listCoaches();
+    const timelist = listTimeZones()
+    const statuslist = listStatus();
+    const contactlist = listContact();
     document.getElementById("showDetails").innerHTML = "Hide Details";
     document.getElementById("result").innerHTML = coachlist;
     document.getElementById("time").innerHTML = timelist;
     document.getElementById("status").innerHTML = statuslist;
     document.getElementById("contact").innerHTML = contactlist;
+    intervalId = setInterval(() => {
+      document.getElementById("time").innerHTML = listTimeZones();
+      document.getElementById("status").innerHTML = listStatus();
+    }, 1000);
+
+    // This to change the text in the button when clicked
   } else {
     // This to change the text in the button when clicked
     document.getElementById("showDetails").innerHTML = "Show Details";
@@ -156,8 +175,10 @@ function displayLists(btn) {
     document.getElementById("time").innerHTML = "";
     document.getElementById("status").innerHTML = "";
     document.getElementById("contact").innerHTML = "";
+    clearInterval(intervalId);
   }
 }
+
 
 function listCoaches() {
   let coachlist = "<ul class='list-group'>";
@@ -173,7 +194,7 @@ function listTimeZones() {
   coaches.forEach((value, index) => {
     timelist += `<li class='list-group-item'>${displayTimeNow(index)}${
       value.UTC
-    }</li>`;
+      }</li>`;
   });
   timelist += "</ul>";
   return timelist;
@@ -198,17 +219,19 @@ function listContact() {
 }
 
 function displayTimeNow(i) {
-  var d = new Date();
-  d.setHours(d.getHours() + coaches[i]["diff"]);
-  var n = d.toUTCString();
+  let date = new Date();
+  date.setHours(date.getHours() + coaches[i]["diff"].hours);
+  date.setMinutes(date.getMinutes() + coaches[i]["diff"].minutes);
+  var n = date.toUTCString();
 
   return n;
 }
 
 //Check if time if available
 function checkStatus(i) {
-  var date = new Date();
-  date.setHours(date.getHours() + coaches[i]["diff"]);
+  let date = new Date();
+  date.setHours(date.getHours() + coaches[i]["diff"].hours);
+  date.setMinutes(date.getMinutes() + coaches[i]["diff"].minutes);
   var hour = date.getUTCHours();
 
   if (hour >= coaches[i]["available"][0] && hour < coaches[i]["available"][1]) {
@@ -224,7 +247,7 @@ function contactLink(i) {
   var hour = date.getUTCHours();
   var html = `
             <li class='list-group-item text-info'>
-                <a href= ${coaches[i]["slack"]} target="_blank">
+                <a href= ${coaches[i]["slack"].profileSearchUrl}${coaches[i]["slack"].userId} target="_blank">
                     Contact ${coaches[i]["name"]} on <i class="fab fa-slack"></i>
                 </a>
             </li>
@@ -255,10 +278,10 @@ function coachesGal() {
 
 function openProfile(btn, i) {
   let tabName = document.getElementsByClassName('tabname');
-    for (let i = 0; i < tabName.length; i++) {
-        if (tabName[i].classList.contains('active') == true){
-          tabName[i].classList.remove('active');
-        }
+  for (let i = 0; i < tabName.length; i++) {
+    if (tabName[i].classList.contains('active') == true) {
+      tabName[i].classList.remove('active');
+    }
   }
   btn.classList.toggle("active");
   document.getElementById("tabcontent").innerHTML = `
