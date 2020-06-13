9a03d01dc19bc131f40d623d72615d0d05218d72
Tatyana Gonzaga <tmugonzaga@gmail.com>
2020-06-14 02:27:01 +0800

    full site
    - files compressed
    - made v1 files (html css js)

--- script-v1.js
+++ script-v1.js
@@ -35,15 +35,78 @@ displayTimeNow = (coach) => {
     return n;
 };
 
+displayCoachSlide = () => {
+    setTimeout(() => {
+      var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        spaceBetween: 0,
+        autoplay: 3000,
+        speed: 800,
+  
+        // If we need pagination
+        pagination: {
+          el: ".swiper-pagination",
+        },
+  
+        // Navigation arrows
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+  
+        // And if we need scrollbar
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+      });
+    }, 0);
+    let slidesHTML = "";
+    coaches.forEach((coach) => {
+      slidesHTML += `
+          <div class="p-5 swiper-slide">
+            <div class="coach-info-container">
+              <div class="profile-picture" style="background-image: url(${coach.profpic})"></div>
+              <div class="coach-info">
+                <h1>${coach.name}</h1>
+                <p>${coach.description}</p>
+              </div>
+            </div>
+            <div class="skill-container">
+                <h2>Statistics</h2>
+                <p><span>Skills:</span> ${coach.skills}</p>
+                <p><span>Rating:</span> ${coach.rating}</p>
+                <p><span>Clients:</span> ${coach.clients}</p>
+            </div>
+          </div>
+      `;
+    });
+    document.querySelector(".swiper-wrapper").innerHTML = slidesHTML;
+};
+
+scrollBar = () => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+};
+
 
 let intervalId;
 window.onload = function () {
+    showCoaches();
+    AOS.refresh();
+    intervalId = setInterval(() => showCoaches(), 1000);
+    displayCoachSlide();
+};
 
-  showCoaches();
-  AOS.refresh();
-  intervalId = setInterval(() => showCoaches(), 1000);
+window.onscroll = function () {
+    scrollBar();
 };
 
 window.onbeforeunload = () => {
-  clearInterval(intervalId);
+    clearInterval(intervalId);
 };
